db5b2f130a1912e5155d29d9bd57eccf5829b212
DarkWanderer <elquentaro@gmail.com>
2019-03-17 22:51:15 +0300

    Revert to StrictHostKeyChecking=no

--- ssh/entrypoint.sh
+++ ssh/entrypoint.sh
@@ -19,11 +19,4 @@ echo run: ssh-add
 eval $(ssh-agent)
 ssh-add "$SSH_PATH/deploy_key"
 
-echo run: ssh-keygen
-ssh-keygen -r $HOST -p ${PORT:-22} -E sha256
-
-echo run: ssh-keyscan
-ssh-keyscan -p ${PORT:-22} $HOST > "$SSH_PATH/known_hosts"
-cat "$SSH_PATH/known_hosts"
-
-ssh -v -tt -p ${PORT:-22} $USER@$HOST "$*"
+ssh -v -o 'StrictHostKeyChecking=no' -tt -p ${PORT:-22} $USER@$HOST "$*"
