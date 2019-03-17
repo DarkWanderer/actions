e7fbd92131e83b17237b93b2e3bf0636153a4515
DarkWanderer <elquentaro@gmail.com>
2019-03-17 12:54:06 +0300

    Switch to using env variables for SSH key

--- git-push/entrypoint.sh
+++ git-push/entrypoint.sh
@@ -7,18 +7,14 @@ SSH_PATH="$HOME/.ssh"
 mkdir "$SSH_PATH"
 touch "$SSH_PATH/known_hosts"
 
-echo "$PRIVATE_KEY" > "$SSH_PATH/deploy_key"
-echo "$PUBLIC_KEY" > "$SSH_PATH/deploy_key.pub"
-
 chmod 700 "$SSH_PATH"
 chmod 600 "$SSH_PATH/known_hosts"
-chmod 600 "$SSH_PATH/deploy_key"
-chmod 600 "$SSH_PATH/deploy_key.pub"
 
 eval $(ssh-agent)
-ssh-add "$SSH_PATH/deploy_key"
+ssh-add <($PRIVATE_KEY)
+ssh-add -l
 
-ssh-keyscan $HOST >> "$SSH_PATH/known_hosts"
+ssh-keygen -R $HOST
 
 echo git push ssh://$USER@$HOST:${PORT:-22}/opt/deploy/$REPO
 git push ssh://$USER@$HOST:${PORT:-22}/opt/deploy/$REPO
