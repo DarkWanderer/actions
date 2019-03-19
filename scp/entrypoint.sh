a29b5c7a07cd6c3796723470e7b65ac2e211229f
DarkWanderer <elquentaro@gmail.com>
2019-03-19 23:16:01 +0300

    Added scp action

---
+++ scp/entrypoint.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+set -e
+
+SSH_PATH="$HOME/.ssh"
+
+mkdir -p "$SSH_PATH"
+touch "$SSH_PATH/known_hosts"
+
+echo "$PRIVATE_KEY" > "$SSH_PATH/deploy_key"
+echo "$PUBLIC_KEY" > "$SSH_PATH/deploy_key.pub"
+
+chmod 700 "$SSH_PATH"
+chmod 600 "$SSH_PATH/known_hosts"
+chmod 600 "$SSH_PATH/deploy_key"
+chmod 600 "$SSH_PATH/deploy_key.pub"
+
+echo run: ssh-add
+eval $(ssh-agent)
+ssh-add "$SSH_PATH/deploy_key"
+
+scp -o 'StrictHostKeyChecking=no' -p ${PORT:-22} "$*"
